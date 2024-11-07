91134d8478d42d197a938827cd995e83e8a4e704
TN108 <24280040@lums.edu.pk>
2024-11-07 13:55:47 +0500

    Add files via upload

---
+++ Jarvis/musicLibrary.py
@@ -0,0 +1,6 @@
+music = {
+    "stealth": "https://youtu.be/U47Tr9BB_wE?si=lSiodVeALz_ogFNF",
+    "march": "https://www.youtube.com/watch?v=txPuYdvImcY&list=OLAK5uy_noVUQI9EwXC2-GaZvs1zZCQhS-tjBW1Zo",
+    "angel": "https://www.youtube.com/watch?v=Uj_-U1qDL-U&list=OLAK5uy_mM1DBO2HeOQs5dLVgPg47cMVryir7gtNk",
+    "skyfall": "https://www.youtube.com/watch?v=DeumyOzKqgI"
+}
